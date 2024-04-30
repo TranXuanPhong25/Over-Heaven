c4a3c3fb24d97d52a470bb1a4ba47e963d64d6e4
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-04-30 12:58:03 +0700

    update gameloop and beta setLogicalSize

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -3,6 +3,9 @@ Game::Game() : window_(NULL), ren_(NULL)
 {
 	state_machine_ = StateMachine::get();
 	state_machine_->setInitialState(IntroState::get());
+	curFrame = 0;
+	preFrame = 0;
+	time_step_ = TARGET_TIMESTEP;
 }
 Game::~Game()
 {
@@ -25,13 +28,7 @@ bool Game::initWindow()
 		std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
 		return false;
 	}
-
-	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
-	{
-		std::cout << "Warning: Linear texture filtering not enabled!" << std::endl;
-	}
-
-	window_ = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	window_ = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP|SDL_WINDOW_ALLOW_HIGHDPI);
 	if (window_ == NULL)
 	{
 		std::cout << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
@@ -40,7 +37,6 @@ bool Game::initWindow()
 
 	SDL_ShowCursor(SDL_DISABLE);
 	SDL_SetWindowIcon(window_, IMG_Load("assets/img/icon64x64.png"));
-	SDL_SetWindowFullscreen(window_, SDL_WINDOW_FULLSCREEN_DESKTOP);
 
 	ren_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren_ == NULL)
@@ -48,7 +44,11 @@ bool Game::initWindow()
 		std::cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
 		return false;
 	}
-
+	if (SDL_RenderSetLogicalSize(ren_, SCREEN_WIDTH, SCREEN_HEIGHT) != 0)
+	{
+		std::cerr << "SDL_RenderSetLogicalSize Error: " << SDL_GetError() << std::endl;
+	}
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "2");
 	int imgFlags = IMG_INIT_PNG;
 	if (!(IMG_Init(imgFlags) & imgFlags))
 	{
@@ -64,41 +64,42 @@ void Game::run()
 	if (initWindow())
 	{
 		state_machine_->getCurrentState()->enter(ren_);
-		Uint64 preFrame = SDL_GetTicks64();
-		Uint64 curFrame;
-		float dT = TARGET_TIMESTEP;
-
+		preFrame = SDL_GetTicks64();
 		SDL_Event e;
 		while (state_machine_->getCurrentState() != ExitState::get())
 		{
-			while (SDL_PollEvent(&e))
-			{
-				if (e.type == SDL_QUIT)
-				{
-					state_machine_->setNextState(ExitState::get());
-					break;
-				}
-				state_machine_->getCurrentState()->handleEvent(e);
-			}
-
-			state_machine_->changeState(ren_);
-			curFrame = SDL_GetTicks64();
-			dT = static_cast<float>(curFrame - preFrame);
-			if (dT < TARGET_TIMESTEP)
-			{
-				SDL_Delay(static_cast<Uint32>(TARGET_TIMESTEP - dT));
-			}
-			else
-			{
-				dT = TARGET_TIMESTEP;
-			}
-			preFrame = curFrame;
-
-			state_machine_->getCurrentState()->update(dT / 1000.0f);
-
-			SDL_RenderClear(ren_);
-			state_machine_->getCurrentState()->render(ren_);
-			SDL_RenderPresent(ren_);
+			launchGameLoop(e);
 		}
 	}
+}
+void Game::launchGameLoop(SDL_Event &e)
+{
+	while (SDL_PollEvent(&e))
+	{
+		if (e.type == SDL_QUIT)
+		{
+			state_machine_->setNextState(ExitState::get());
+			break;
+		}
+		state_machine_->getCurrentState()->handleEvent(e);
+	}
+
+	state_machine_->changeState(ren_);
+	curFrame = SDL_GetTicks64();
+	time_step_ = static_cast<float>(curFrame - preFrame);
+	if (time_step_ < TARGET_TIMESTEP)
+	{
+		SDL_Delay(static_cast<Uint32>(TARGET_TIMESTEP - time_step_));
+	}
+	else
+	{
+		time_step_ = TARGET_TIMESTEP;
+	}
+	preFrame = curFrame;
+
+	state_machine_->getCurrentState()->update(time_step_ / 1000.0f);
+
+	SDL_RenderClear(ren_);
+	state_machine_->getCurrentState()->render(ren_);
+	SDL_RenderPresent(ren_);
 }
--- src/core/Game.h
+++ src/core/Game.h
@@ -8,10 +8,15 @@ public:
 	Game();
 	~Game();
 	void run();
+	void launchGameLoop(SDL_Event &e);
 private:
 	bool initWindow();
 	SDL_Renderer* ren_;
 	SDL_Window* window_;
 	StateMachine* state_machine_;
+
+	Uint64 preFrame;
+	Uint64 curFrame;
+	float time_step_;
 };
 #endif
